1ec5e1cbb0a1424b2e8e49200f69bf783ad14074
Anna <hayes.anna000@gmail.com>
2025-01-17 13:16:43 -0500

    finish bastard

--- scripts/4_World/classes/playermodifiers/modifiers/saucebastardmdfr.c
+++ scripts/4_World/classes/playermodifiers/modifiers/saucebastardmdfr.c
@@ -1,14 +1,22 @@
 class sauce_BastardMdfr : sauce_BaseMdfr
 {
-    //todo add morphine effect
+    float damage = 1; //damage per second
     override void Init()
     {
         m_ID = blobModifiersGoonshine.MDF_GOONSHINE_BASTARD;
-
+        
         DisableActivateCheck();
+
+        m_RegenTime = 30000; //time in ms
     }
 
+    override void OnTick(PlayerBase player, float deltaT)
+    {
+        player.AddHealth("GlobalHealth", "Health", -(damage * deltaT))
+    }
 
-
-
+    override void OnActivate(PlayerBase player)
+    {
+        player.GetTransferValues().ReceiveValue(TransferValues.TYPE_RADX, -1000);
+    }
 }
